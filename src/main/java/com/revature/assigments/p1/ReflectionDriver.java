b9c0c6c0cec6293138f6f10d24e9a4a25b85d1d6
ozzy castillo <oswaldo.castillo@revature.net>
2021-05-17 20:40:43 -0500

    Testing reading class with predicate check

--- src/main/java/com/revature/assigments/p1/ReflectionDriver.java
+++ src/main/java/com/revature/assigments/p1/ReflectionDriver.java
@@ -1,6 +1,7 @@
 package com.revature.assigments.p1;
 
 import com.revature.assigments.p1.models.AppUser;
+import com.revature.assigments.p1.util.Entity;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -9,6 +10,7 @@ import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Predicate;
 
 public class ReflectionDriver {
     public static void main(String[] args){
@@ -31,8 +33,25 @@ public class ReflectionDriver {
             e.printStackTrace();
         }
 
+        System.out.println("+------------------------------------+");
         try{
-            driver.getClassesInPackage("com.revature.assigments.p1.models");
+            List<Class<?>> entityClasses = driver.getClassesInPackage("com.revature.assigments.p1.models");
+            for(Class<?>  entity : entityClasses){
+                System.out.println(entity);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("+------------------------------------+");
+        try{
+            String packageName = "com.revature.assigments.p1.models";
+            List<Class<?>> entityClassesWithConstraints = driver.getClassesInPackageWithConstraints(packageName, clazz -> clazz.isAnnotationPresent(Entity.class));
+            for(Class<?>  entityWithConstraints : entityClassesWithConstraints ){
+                System.out.println(entityWithConstraints);
+            }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
@@ -93,6 +112,33 @@ public class ReflectionDriver {
             packageClasses.add(ucl.loadClass(packageName+"."+className.substring(0,className.length()-6)));
         }
 
+
+        return packageClasses;
+    }
+
+    public List<Class<?>> getClassesInPackageWithConstraints(String packageName, Predicate<Class<?>> predicate) throws MalformedURLException, ClassNotFoundException {
+        List<Class<?>> packageClasses = new ArrayList<>();
+        List<String> classNames = new ArrayList<>();
+
+        File packageDirectory = new File("target/classes/" + packageName.replace('.','/'));
+
+        for(File file : Objects.requireNonNull(packageDirectory.listFiles())){
+            if(file.isDirectory()){
+                packageClasses.addAll(getClassesInPackageWithConstraints(packageName + "target/classes/",predicate));
+            }else if(file.getName().contains(".class")){
+                classNames.add(file.getName());
+            }
+
+        }
+
+        URLClassLoader ucl = new URLClassLoader(new URL[]{new File("target/classes.").toURI().toURL()});
+
+        for(String className : classNames){
+            Class<?> clazz = ucl.loadClass(packageName + "." + className.substring(0,className.length()-6));
+            if(predicate.test(clazz)){
+                packageClasses.add(clazz);
+            }
+        }
 
         return packageClasses;
     }
