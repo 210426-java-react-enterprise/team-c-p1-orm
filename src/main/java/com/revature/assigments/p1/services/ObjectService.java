c418d62ccb9d36ac696667ba5f8a38495ad84550
ozzy castillo <oswaldo.castillo@revature.net>
2021-05-25 19:21:41 -0500

    BringInstance Method > ObjectService

--- src/main/java/com/revature/assigments/p1/services/ObjectService.java
+++ src/main/java/com/revature/assigments/p1/services/ObjectService.java
@@ -97,7 +97,7 @@ public class ObjectService {
             //3.-Calling the DAO method to populate the Instance Map
 
             objectDao.requestInstanceData(conn, object, objectId,objectMapSequence, objectMapped);
-            //4.-Call the Object Mapper to populate the new Instance.
+            //4.-Call the Object Mapper to populate the new Instance.O
             //After to talk with Wezley the only way to update my new instance is through reflection
 
             //ChanMethod setIdMethod = object.getClass().getMethod("set")
